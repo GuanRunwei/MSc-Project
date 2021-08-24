f86dbaee6b8cd7e7a9a5e5eb7f5cabc5a4b7dbe4
Runwei Guan <31555628+GuanRunwei@users.noreply.github.com>
2021-08-24 14:10:47 +0800

    Add files via upload

---
+++ Client/Helper/PictureInterceptor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pathological_section_diagnosis_assistant_software.Helper
+{
+    class PictureInterceptor
+    {
+        #region 图片数据类型限制
+        public static string[] LimitPictureType = { "png", "jpg", "gif", "jpeg", "bmp", "ico" };
+        #endregion
+
+        //public bool JudgePictures(IFormFileCollection files)
+        //{
+        //    foreach (var file in files)
+        //    {
+        //        Console.WriteLine(Path.GetExtension(file.FileName).ToLower());
+        //        if (!LimitPictureType.Contains(Path.GetExtension(file.FileName).ToLower().Substring(1)))
+        //            return false;
+        //    }
+        //    return true;
+        //}
+    }
+}
